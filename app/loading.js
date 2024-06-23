664a44f7f7bb1286c0d1357db08edf84f311d5ea
SUJAY GOWDA <sujay.gowda@i4sight.net>
2024-06-23 21:23:59 +0530

    client component rendering and loading state indicator

---
+++ app/loading.js
@@ -0,0 +1,5 @@
+// this component will be rendered for the parent and child components when the data is being fetched from the server
+
+export default function loading() {
+  return <div>Loading data...</div>;
+}
