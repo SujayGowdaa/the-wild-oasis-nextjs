48684fbb3c293ae0c16512b873b5ac5707cff684
SUJAY GOWDA <sujay.gowda@i4sight.net>
2024-06-27 16:11:45 +0530

    stream UI with suspense: cabinlist

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,15 +1,15 @@
-import CabinCard from '../_components/CabinCard';
-import { getCabins } from '../_lib/data-service';
+import { Suspense } from 'react';
+
+import CabinList from '../_components/CabinList';
+import Spinner from '../_components/Spinner';
 
 export const metadata = {
   title: 'cabins',
 };
 
-export default async function Page() {
-  const cabins = await getCabins();
-
+export default function Page() {
   return (
-    <div>
+    <div className=' h-full'>
       <h1 className='text-4xl mb-5 text-accent-400 font-medium'>
         Our Luxury Cabins
       </h1>
@@ -21,14 +21,15 @@ export default async function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-
-      {cabins.length > 0 && (
-        <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14'>
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense
+        fallback={
+          <div className=' h-full w-full flex justify-center items-center'>
+            <Spinner />
+          </div>
+        }
+      >
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
--- app/loading.js
+++ app/loading.js
@@ -1,5 +1,6 @@
 // this component will be rendered for the parent and child components when the data is being fetched from the server
+import Spinner from './_components/Spinner';
 
 export default function loading() {
-  return <div>Loading data...</div>;
+  return <Spinner />;
 }
