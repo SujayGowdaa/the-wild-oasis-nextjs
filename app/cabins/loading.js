4b57dc0be6a22a2017a92b35392b1d81f08acbc5
SUJAY GOWDA <sujay.gowda@i4sight.net>
2024-06-27 16:25:48 +0530

    cabin details page using dynamic route

--- app/cabins/loading.js
+++ app/cabins/loading.js
@@ -1,11 +1,12 @@
 import Spinner from '../_components/Spinner';
 
 export default function loading() {
-  return null;
-  // <div className=' flex h-full w-full justify-center items-center'>
-  //   <div>
-  //     <Spinner />
-  //     <p className=' text-xl text-primary-200'>Loading Cabin Data...</p>
-  //   </div>
-  // </div>
+  return (
+    <div className=' flex h-full w-full justify-center items-center'>
+      <div>
+        <Spinner />
+        <p className=' text-xl text-primary-200'>Loading Cabin Data...</p>
+      </div>
+    </div>
+  );
 }
