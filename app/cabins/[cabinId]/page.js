d8c054143170c2f498c8291ed81df6503360bb6c
SUJAY GOWDA <sujay.gowda@i4sight.net>
2024-06-28 14:34:07 +0530

    generate static pages for dynamic routes [cabinId]

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,4 +1,4 @@
-import { getCabin } from '@/app/_lib/data-service';
+import { getCabin, getCabins } from '@/app/_lib/data-service';
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from '@heroicons/react/24/solid';
 import Image from 'next/image';
 
@@ -7,6 +7,24 @@ export async function generateMetadata({ params }) {
   return {
     title: `Cabin ${name}`,
   };
+}
+
+/* The generateStaticParams function provides the list of cabinId values for which to generate static pages.
+Next.js will pre-generate a static page for each cabinId returned by generateStaticParams.
+When a user visits a URL like /cabins/1, Next.js will serve the pre-generated page for cabinId 1, improving performance and SEO. */
+
+export async function generateStaticParams() {
+  // Retrieve the list of cabins. This could be an API call, database query, etc.
+  const cabins = await getCabins();
+
+  // Map over the cabins to create an array of objects with a `cabinId` property.
+  // Each `cabinId` is converted to a string.
+  const IDs = cabins.map((cabin) => ({
+    cabinId: String(cabin.id),
+  }));
+
+  // Return the array of objects. This will be used by Next.js to generate static paths.
+  return IDs;
 }
 
 export default async function Page({ params }) {
@@ -28,14 +46,11 @@ export default async function Page({ params }) {
             alt={`Cabin ${name}`}
           />
         </div>
-
         <div>
           <h3 className='text-accent-100 font-black text-7xl mb-5 translate-x-[-254px] bg-primary-950 p-6 pb-1 w-[150%]'>
             Cabin {name}
           </h3>
-
           <p className='text-lg text-primary-300 mb-10'>{description}</p>
-
           <ul className='flex flex-col gap-4 mb-7'>
             <li className='flex gap-3 items-center'>
               <UsersIcon className='h-5 w-5 text-primary-600' />
