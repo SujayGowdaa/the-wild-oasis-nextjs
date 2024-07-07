b5c284d5bcd0a3384f8f5c9aa19a95f617dfed16
SUJAY GOWDA <sujay.gowda@i4sight.net>
2024-07-07 15:10:31 +0530

    get user session using google provider

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,15 +1,26 @@
+import { auth } from '../_lib/auth';
 import { getBookedDatesByCabinId, getSettings } from '../_lib/data-service';
 import DateSelector from './DateSelector';
+import LoginMessage from './LoginMessage';
 import ReservationForm from './ReservationForm';
 
 export default async function Reservation({ cabin }) {
+  const session = await auth();
   const settings = await getSettings();
   const bookedDates = await getBookedDatesByCabinId(cabin.id);
 
   return (
     <div className=' grid grid-cols-2 border border-primary-800 min-h-[400px]'>
       <DateSelector settings={settings} cabin={cabin} />
-      <ReservationForm bookedDates={bookedDates} cabin={cabin} />
+      {session?.user ? (
+        <ReservationForm
+          bookedDates={bookedDates}
+          cabin={cabin}
+          user={session.user}
+        />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,16 +1,14 @@
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
 
-export const authConfig = {
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.SECRET,
-    }),
-  ],
-};
-
 export const {
   auth,
   handlers: { GET, POST },
-} = NextAuth(authConfig);
+} = NextAuth({
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+});
